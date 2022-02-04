0941aa175396673526e81e0c154bb485efa3c219
Sam Sycamore <samuelsycamore@gmail.com>
2022-02-03 22:45:09 -0600

    adds MuiNextLink, Header component

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import createEmotionCache from '../styles/createEmotionCache';
+import Header from '@components/Header';
 
 import '/styles/globals.css';
 import theme from '../styles/theme';
@@ -22,6 +23,7 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
